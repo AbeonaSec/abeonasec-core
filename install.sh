1eb59fe0a996db52be116fb5771a6e4a26d1ec89
powerfuljackell <johnsonjakel@aol.com>
2026-02-27 23:47:43 +0000

    Updated install script for prereqs

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update && sudo apt-get upgrade
 
+sudo ubuntu-drivers autoinstall
+
 sudo useradd -r -s /bin/bash abeonasec
 
 #check for dependencies
@@ -42,6 +44,13 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
 && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
 sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
 sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.18.2-1
+sudo apt-get install -y \
+nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
 
 sudo mkdir /etc/abeonasec/
 sudo mkdir /opt/abeonasec/
